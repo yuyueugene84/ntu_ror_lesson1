8c5589bbff079830c0f908d08a18f36f7652065e
yuyueugene84 <yuyueugene84@gmail.com>
2015-11-13 08:35:35 +0800

    added calculator, loop and user input programs

---
+++ loop.rb
@@ -0,0 +1,36 @@
+#loop
+
+i = 0
+
+loop do 
+  puts "looped to #{i}"	
+
+  i += 1
+
+  if i == 10
+  	break
+  end
+  
+end
+
+#while loop    
+x = 10
+
+while x > 0 
+  
+  puts "while loop to #{x}"
+  
+  x -= 1
+
+end
+
+#until loop
+y = 10
+
+until y == 0
+
+  puts "until loop to #{y}"
+  
+  y -= 1
+
+end
---
+++ user_input.rb
@@ -0,0 +1,13 @@
+puts "Please enter X to quit"
+
+#get inpur from user
+input = gets.chomp 
+
+while input != "X"
+  puts "Please enter X to quit"
+  input = gets.chomp 
+end
+
+
+
+
